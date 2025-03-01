f99859f99cd5153d9fd78ac6711918564b8bd92a
NEELESH <146578707+Neeleshyd@users.noreply.github.com>
2025-03-01 14:39:32 +0000

    variable

---
+++ .devcontainer/basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId =7897
+let accountEmail="neelesh@gmail.com"
+var accountPassword ="4545"
+accountCity ="lucknow"
+let accountState;
+//accountId=2
+
+accountEmail="yadav@gmail.com"
+
+var accountPassword ="0000"
+
+accountCity ="delhi"
+
+console.log(accountId);
+console.table([accountId,accountState,accountPassword,accountEmail,accountCity])
