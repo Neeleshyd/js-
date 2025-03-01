a04d382c00c92318be533126631a2f8cf6c89acf
NEELESH <146578707+Neeleshyd@users.noreply.github.com>
2025-03-01 14:37:44 +0000

    conversion

---
+++ .devcontainer/basics/03_conversion.js
@@ -0,0 +1,17 @@
+let str1="hello"
+let str2=" Neelesh"
+
+let str3=str1+str2
+
+//console.log(str3);
+
+//console.log("1"+2);//12
+
+//console.log(1+"2");//12
+//console.log("1"+2+2);//122
+//console.log(1+2+"2");//32
+
+console.log(+true);
+console.log(+ "");
+
+
